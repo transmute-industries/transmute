a84f80fed6ba4b4c0e9072b6f8ddae3a38230ff2
Orie Steele <orie@ohs.io>
2017-11-27 22:43:19 -0600

    implement a simple dapp package manager that only works in lerna atm

---
+++ packages/transmute-pm/lib/index.js
@@ -0,0 +1,9 @@
+// Transmute Avatar
+// A package manager for typescript dapps and oracles built on top of npm.
+
+module.exports = {
+  publish: require("./publish"),
+  list: require("./list"),
+  install: require("./install"),
+  com: require("./common")
+};
