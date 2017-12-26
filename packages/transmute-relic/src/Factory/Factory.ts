8a3445cf7a8fd4652d84fd210eaec922626f266b
Orie Steele <orie@ohs.io>
2017-12-26 12:21:07 -0600

    clean

--- packages/transmute-relic/src/Factory/Factory.ts
+++ packages/transmute-relic/src/Factory/Factory.ts
@@ -1,11 +1,11 @@
-import { UnsafeEventStoreFactory } from '../types/UnsafeEventStoreFactory'
-import { EventStoreFactory } from '../types/EventStoreFactory'
-import { W3 } from 'soltsice'
+import { UnsafeEventStoreFactory } from "../types/UnsafeEventStoreFactory";
+import { EventStoreFactory } from "../types/EventStoreFactory";
+import { W3 } from "soltsice";
 
-import { Adapter } from '../Store/Adapter'
+import { Adapter } from "../Store/Adapter";
 
 export namespace Factory {
-  export type GenericFactory = UnsafeEventStoreFactory | EventStoreFactory
+  export type GenericFactory = UnsafeEventStoreFactory | EventStoreFactory;
 
   export enum Types {
     UnsafeEventStoreFactory,
@@ -18,32 +18,35 @@ export namespace Factory {
   export const typeClassMapper = (name: Types) => {
     switch (name) {
       case Types.UnsafeEventStoreFactory:
-        return UnsafeEventStoreFactory
+        return UnsafeEventStoreFactory;
       default:
-        return EventStoreFactory
+        return EventStoreFactory;
     }
-  }
+  };
 
   /**
    * Factory create
    */
   export const create = async (type: Types, web3: any, fromAddress: string) => {
-    W3.Default = web3
-    const factoryClass = typeClassMapper(type)
+    W3.Default = web3;
+    const factoryClass = typeClassMapper(type);
     const instance = await factoryClass.New(W3.TC.txParamsDefaultDeploy(fromAddress), {
       _multisig: fromAddress
-    })
-    return instance
-  }
+    });
+    return instance;
+  };
 
   /**
    * Factory createStore
    */
-  export const createStore = async (factory: GenericFactory, adapter: Adapter, web3: any, fromAddress: string) => {
-    W3.Default = web3
-    let receipt = await factory.createEventStore(W3.TC.txParamsDefaultDeploy(fromAddress))
-
-    return adapter.extractEventsFromLogs(receipt.logs)
-
-  }
+  export const createStore = async (
+    factory: GenericFactory,
+    adapter: Adapter,
+    web3: any,
+    fromAddress: string
+  ) => {
+    W3.Default = web3;
+    let receipt = await factory.createEventStore(W3.TC.txParamsDefaultDeploy(fromAddress));
+    return adapter.extractEventsFromLogs(receipt.logs);
+  };
 }
