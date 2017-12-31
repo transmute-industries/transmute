4c7b0c077a28456c9ea136c61a5c87a0d5da7f08
Orie Steele <orie@ohs.io>
2017-12-31 13:49:02 -0600

    fix read model tests

--- packages/transmute-relic/src/Store/ReadModel/__tests__/sync.tests.ts
+++ packages/transmute-relic/src/Store/ReadModel/__tests__/sync.tests.ts
@@ -1,51 +1,68 @@
-import { ReadModel } from "../ReadModel";
+import { ReadModel } from '../ReadModel'
 
-import { Store } from "../../";
-import { getSetupAsync } from "../../../__mocks__/setup";
+import { Store } from '../../'
+import { getSetupAsync } from '../../../__mocks__/setup'
 
-import events from "../__mocks__/events";
-import { reducer, initialState } from "../__mocks__/reducer";
+import events from '../__mocks__/events'
+import { reducer, initialState } from '../__mocks__/reducer'
 
-let state = initialState;
+import Relic from '../../../transmute-relic'
+import { EventStoreAdapter } from '../../EventStoreAdapter'
+let state = initialState
+
+import { EventStore } from '../../../SolidityTypes'
 
 /**
  * ReadModel event tests
  */
-describe("ReadModel event tests", () => {
-  const Storage = require("node-storage");
-  const db = new Storage("./read_model_storage");
+describe('ReadModel event tests', () => {
+  const Storage = require('node-storage')
+  const db = new Storage('./read_model_storage')
   const rmAdapter: any = {
     getItem: (id: string) => {
-      return JSON.parse(db.get(id));
+      return JSON.parse(db.get(id))
     },
     setItem: (id: string, value: any) => {
-      return db.put(id, JSON.stringify(value));
+      return db.put(id, JSON.stringify(value))
     }
-  };
+  }
 
-  let setup: any;
+  let setup: any
+  let relic: Relic
+  let accounts: string[]
+  let store: EventStore
+  let eventStoreAdapter: EventStoreAdapter
 
   beforeAll(async () => {
-    setup = await getSetupAsync();
-    state.contractAddress = setup.store.address;
-    state.readModelStoreKey = `${state.readModelType}:${state.contractAddress}`;
-  });
+    setup = await getSetupAsync()
+    state.contractAddress = setup.store.address
+    state.readModelStoreKey = `${state.readModelType}:${state.contractAddress}`
+    relic = setup.relic
+    store = setup.store
+    eventStoreAdapter = setup.eventStoreAdapter
+    accounts = setup.accounts
+  })
 
-  it("can sync", async () => {
-    let { store, adapter, relic, accounts } = setup;
-    let writtenEvents = await Store.writeFSAs(store, adapter, relic.web3, accounts[0], events);
-    expect(writtenEvents.length).toBe(2);
-    let rm = new ReadModel(rmAdapter, reducer, state);
+  it('can sync', async () => {
+    let writtenEvents = await Store.writeFSAs(
+      store,
+      eventStoreAdapter,
+      relic.web3,
+      accounts[0],
+      events
+    )
+    expect(writtenEvents.length).toBe(2)
+    let rm = new ReadModel(rmAdapter, reducer, state)
 
-    let hadChanges = await rm.sync(store, adapter, relic.web3, accounts[0]);
-    expect(hadChanges); // expect changes, we have not colled sync yet
+    let hadChanges = await rm.sync(store, eventStoreAdapter, relic.web3, accounts[0])
+    expect(hadChanges) // expect changes, we have not colled sync yet
 
-    hadChanges = await rm.sync(store, adapter, relic.web3, accounts[0]);
-    expect(!hadChanges); // expect no changes, we called sync, and have not written any events
+    hadChanges = await rm.sync(store, eventStoreAdapter, relic.web3, accounts[0])
+    expect(!hadChanges) // expect no changes, we called sync, and have not written any events
 
-    writtenEvents = await Store.writeFSAs(store, adapter, relic.web3, accounts[0], events);
+    writtenEvents = await Store.writeFSAs(store, eventStoreAdapter, relic.web3, accounts[0], events)
 
-    hadChanges = await rm.sync(store, adapter, relic.web3, accounts[0]);
-    expect(hadChanges); // expect changes, wrote events written any events
-  });
-});
+    hadChanges = await rm.sync(store, eventStoreAdapter, relic.web3, accounts[0])
+    expect(hadChanges) // expect changes, wrote events written any events
+  })
+})
