2127b4e33a449dc01551b58cdcfda8c1e5aa8095
Orie Steele <orie@ohs.io>
2018-01-01 12:18:48 -0600

    ref

---
+++ packages/transmute-framework/src/relic.ts
@@ -0,0 +1,26 @@
+import { W3 } from 'soltsice'
+import { transmuteWeb3, ITransmuteWeb3Config } from './web3'
+
+export default class Relic {
+  web3: W3
+
+  constructor(config: ITransmuteWeb3Config) {
+    this.web3 = transmuteWeb3(config, false)
+  }
+
+  async getAccounts(): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.getAccounts((err: any, accounts: string[]) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(accounts)
+      })
+    }) as Promise<string[]>
+  }
+
+  async getBalance(address: string): Promise<number> {
+    let balance: any = await this.web3.eth.getBalance(address)
+    return balance.toNumber()
+  }
+}
