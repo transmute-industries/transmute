0a10ea86a43ef81a2a092fefebb7dadc2c8b3b42
Orie Steele <orie@ohs.io>
2017-12-31 20:36:14 -0600

    refactor

---
+++ packages/transmute-framework/src/Store/__tests__/eventstore.tests.ts
@@ -0,0 +1,127 @@
+import { getSetupAsync } from '../../__mocks__/setup'
+import { EventStore } from '../../TransmuteContracts/EventStore'
+import { Store } from '../Store'
+
+import Relic from '../../transmute-framework'
+import { W3 } from 'soltsice'
+const Storage = require('node-storage')
+import { EventStoreAdapter } from '../../Store/EventStoreAdapter'
+
+import MarshalledEvents from '../../__mocks__/MarshalledEvents'
+
+import goodEvents from '../__mocks__/good.events'
+
+import * as EventTransformer from '../../Utils/EventTransformer'
+
+const WRITE_EVENT_GAS_COST = 4000000
+/**
+ * Store test
+ */
+describe('Store', () => {
+  let relic: Relic
+  let accounts: string[]
+  let store: EventStore
+  let eventStoreAdapter: EventStoreAdapter
+
+  beforeAll(async () => {
+    let setup = await getSetupAsync()
+    relic = setup.relic
+    store = setup.store
+    accounts = setup.accounts
+    eventStoreAdapter = setup.eventStoreAdapter
+  })
+
+  it('the store rejects FSAs that are not formatted correctly', async () => {
+    let badFSA = {
+      type: 'test',
+      payload: {
+        key: 'value'
+      },
+      meta: {}
+    }
+    try {
+      let fsa = await Store.writeFSA(store, eventStoreAdapter, relic.web3, accounts[0], badFSA)
+    } catch (e) {
+      expect(e.message).toEqual(
+        'fsa.meta.adapter is not defined. be sure to set it when fsa.payload is an object (isAdapterEvent).'
+      )
+    }
+  })
+
+  it('the store writes FSAs that ARE NOT isAdapterEvent ', async () => {
+    // add tests for all !isAdapterEvent types here....
+    let goodFSA = {
+      type: 'test',
+      payload: {
+        key: 'address',
+        value: '0x01000c268181f5d90587392ff67ada1a16393fe4'
+      },
+      meta: {}
+    }
+    let fsa = await Store.writeFSA(store, eventStoreAdapter, relic.web3, accounts[0], goodFSA)
+    expect(fsa.payload.key).toEqual(goodFSA.payload.key)
+    expect(fsa.payload.value).toEqual(goodFSA.payload.value)
+  })
+
+  it('the store writes FSAs that ARE isAdapterEvent ', async () => {
+    // add tests for all !isAdapterEvent types here....
+    let goodFSA = {
+      type: 'test',
+      payload: {
+        cool: 'story',
+        bro: 1
+      },
+      meta: {
+        adapter: 'I'
+      }
+    }
+    let writtenFSA = await Store.writeFSA(
+      store,
+      eventStoreAdapter,
+      relic.web3,
+      accounts[0],
+      goodFSA
+    )
+    expect(writtenFSA.payload).toEqual(goodFSA.payload)
+  })
+
+  it('read and write FSAs', async () => {
+    let setup = await getSetupAsync()
+    let writtenEvents = await Store.writeFSAs(
+      setup.store,
+      setup.eventStoreAdapter,
+      setup.relic.web3,
+      accounts[0],
+      goodEvents
+    )
+    expect(writtenEvents.length).toBe(6)
+    // 3 events created by the factory...
+    let readAllEvents = await Store.readFSAs(
+      setup.store,
+      setup.eventStoreAdapter,
+      setup.relic.web3,
+      accounts[0]
+    )
+    expect(readAllEvents.length).toBe(9)
+  })
+})
+
+// it("sanity", async () => {
+//   let event = MarshalledEvents[0];
+//   let eventCount = await store.eventCount()
+//   expect(eventCount.toNumber()).toBe(3);
+
+//   let eventValues = await store.readEvent(0, W3.TC.txParamsDefaultDeploy(accounts[0]))
+//   let readFSA = EventTransformer.arrayToFSA(eventValues)
+//   console.log(readFSA)
+
+//   let receipt = await store.writeEvent(
+//     event.eventType,
+//     event.keyType,
+//     event.valueType,
+//     event.key,
+//     event.value,
+//     W3.TC.txParamsDefaultDeploy(accounts[0], WRITE_EVENT_GAS_COST)
+//   );
+//   console.log(receipt.logs)
+// });
---
+++ packages/transmute-framework/src/TransmuteContracts/Destructible.ts
@@ -0,0 +1,186 @@
+
+import { W3, SoltsiceContract } from 'soltsice';
+
+/**
+ * Destructible API
+ */
+export class Destructible extends SoltsiceContract {
+    static get Artifacts() { return require('../../node_modules/transmute-contracts/build/contracts/Destructible.json'); }
+
+    static get BytecodeHash() {
+        // we need this before ctor, but artifacts are static and we cannot pass it to the base class, so need to generate
+        let artifacts = Destructible.Artifacts;
+        if (!artifacts || !artifacts.bytecode) {
+            return undefined;
+        }
+        let hash = W3.sha3(JSON.stringify(artifacts.bytecode));
+        return hash;
+    }
+
+    // tslint:disable-next-line:max-line-length
+    static async New(deploymentParams: W3.TC.TxParams, ctorParams?: {}, w3?: W3, link?: SoltsiceContract[]): Promise<Destructible> {
+        let contract = new Destructible(deploymentParams, ctorParams, w3, link);
+        await contract._instancePromise;
+        return contract;
+    }
+
+    static async At(address: string | object, w3?: W3): Promise<Destructible> {
+        let contract = new Destructible(address, undefined, w3, undefined);
+        await contract._instancePromise;
+        return contract;
+    }
+
+    protected constructor(
+        deploymentParams: string | W3.TC.TxParams | object,
+        ctorParams?: {},
+        w3?: W3,
+        link?: SoltsiceContract[]
+    ) {
+        // tslint:disable-next-line:max-line-length
+        super(
+            w3,
+            Destructible.Artifacts,
+            ctorParams ? [] : [],
+            deploymentParams,
+            link
+        );
+    }
+    /*
+        Contract methods
+    */
+    
+    // tslint:disable-next-line:member-ordering
+    public destroy = Object.assign(
+        // tslint:disable-next-line:max-line-length
+        // tslint:disable-next-line:variable-name
+        ( txParams?: W3.TC.TxParams): Promise<W3.TC.TransactionResult> => {
+            return new Promise((resolve, reject) => {
+                this._instance.destroy( txParams || this._sendParams)
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(err));
+            });
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            sendTransaction: ( txParams?: W3.TC.TxParams): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.destroy.sendTransaction( txParams || this._sendParams)
+                        .then((res) => resolve(res))
+                        .catch((err) => reject(err));
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            data: (): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    resolve(this._instance.destroy.request().params[0].data);
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            estimateGas: (): Promise<number> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.destroy.estimateGas().then((g) => resolve(g));
+                });
+            }
+        });
+    
+    // tslint:disable-next-line:max-line-length
+    // tslint:disable-next-line:variable-name
+    public owner( txParams?: W3.TC.TxParams): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this._instance.owner
+                .call( txParams || this._sendParams)
+                .then((res) => resolve(res))
+                .catch((err) => reject(err));
+        });
+    }
+    
+    // tslint:disable-next-line:member-ordering
+    public transferOwnership = Object.assign(
+        // tslint:disable-next-line:max-line-length
+        // tslint:disable-next-line:variable-name
+        (newOwner: string, txParams?: W3.TC.TxParams): Promise<W3.TC.TransactionResult> => {
+            return new Promise((resolve, reject) => {
+                this._instance.transferOwnership(newOwner, txParams || this._sendParams)
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(err));
+            });
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            sendTransaction: (newOwner: string, txParams?: W3.TC.TxParams): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.transferOwnership.sendTransaction(newOwner, txParams || this._sendParams)
+                        .then((res) => resolve(res))
+                        .catch((err) => reject(err));
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            data: (newOwner: string): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    resolve(this._instance.transferOwnership.request(newOwner).params[0].data);
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            estimateGas: (newOwner: string): Promise<number> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.transferOwnership.estimateGas(newOwner).then((g) => resolve(g));
+                });
+            }
+        });
+    
+    // tslint:disable-next-line:member-ordering
+    public destroyAndSend = Object.assign(
+        // tslint:disable-next-line:max-line-length
+        // tslint:disable-next-line:variable-name
+        (_recipient: string, txParams?: W3.TC.TxParams): Promise<W3.TC.TransactionResult> => {
+            return new Promise((resolve, reject) => {
+                this._instance.destroyAndSend(_recipient, txParams || this._sendParams)
+                    .then((res) => resolve(res))
+                    .catch((err) => reject(err));
+            });
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            sendTransaction: (_recipient: string, txParams?: W3.TC.TxParams): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.destroyAndSend.sendTransaction(_recipient, txParams || this._sendParams)
+                        .then((res) => resolve(res))
+                        .catch((err) => reject(err));
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            data: (_recipient: string): Promise<string> => {
+                return new Promise((resolve, reject) => {
+                    resolve(this._instance.destroyAndSend.request(_recipient).params[0].data);
+                });
+            }
+        },
+        {
+            // tslint:disable-next-line:max-line-length
+            // tslint:disable-next-line:variable-name
+            estimateGas: (_recipient: string): Promise<number> => {
+                return new Promise((resolve, reject) => {
+                    this._instance.destroyAndSend.estimateGas(_recipient).then((g) => resolve(g));
+                });
+            }
+        });
+    
+}
