289b143377fd6de2696b67f2166377da345e0f2b
Orie Steele <orie@ohs.io>
2017-12-31 19:29:49 -0600

    update supported external event types

---
+++ packages/transmute-relic/src/Store/__mocks__/good.events.ts
@@ -0,0 +1,54 @@
+export default [
+  {
+    type: 'test',
+    payload: {
+      key: 'FOOasdfasdf123',
+      value: 'FOOasdf123123123'
+    },
+    meta: {}
+  },
+  {
+    type: 'test',
+    payload: {
+      key: 'value',
+      key2: 'value2'
+    },
+    meta: {
+      adapter: 'I'
+    }
+  },
+  {
+    type: 'test',
+    payload: {
+      key: 'value',
+      key2: 'value2'
+    },
+    meta: {
+      adapter: 'N'
+    }
+  },
+  {
+    type: 'test',
+    payload: {
+      key: 'address',
+      value: '0x01000c268181f5d90587392ff67ada1a16393fe4'
+    },
+    meta: {}
+  },
+  {
+    type: 'test',
+    payload: {
+      key: 'bytes32',
+      value: '0x000000000000000000000000000000000000000000000000000000000000000a'
+    },
+    meta: {}
+  },
+  {
+    type: 'test',
+    payload: {
+      key: 'uint',
+      value: 0
+    },
+    meta: {}
+  }
+]
