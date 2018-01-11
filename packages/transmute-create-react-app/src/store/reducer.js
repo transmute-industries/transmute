6f2c0b072f48d31b4aaab883a6c5250aff3be61b
Orie Steele <orie@ohs.io>
2018-01-10 19:57:00 -0600

    add create-react-app minimal example

--- packages/transmute-create-react-app/src/store/reducer.js
+++ packages/transmute-create-react-app/src/store/reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
 import { reducer as search } from './search/reducer'
+import { reducer as transmute } from './transmute/reducer'
 
 export default combineReducers({
   routing: routerReducer,
-  search
+  search,
+  transmute
 })
