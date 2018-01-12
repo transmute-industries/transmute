3139ae29d442549b602a46da7cad44e067c6d303
Orie Steele <orie@ohs.io>
2018-01-12 17:39:34 -0600

    refactor transmute ipfs

--- packages/transmute-ipfs/test.js
+++ packages/transmute-ipfs/test.js
@@ -1,48 +1,11 @@
 const path = require("path");
-
-var fs = require("fs");
-
-var ipfsAPI = require("ipfs-api");
-
-// connect to ipfs daemon API server
-var ipfs = ipfsAPI("localhost", "5001", { protocol: "http" });
-// leaving out the arguments will default to these values
-
-const readFile = filePath => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", function(err, contents) {
-      if (err) {
-        reject(err);
-      }
-      resolve(contents);
-    });
-  });
-};
-
-const addDir = async (directoryPath, ignoreList) => {
-  return new Promise((resolve, reject) => {
-    ipfs.util.addFromFs(
-      directoryPath,
-      { recursive: true, ignore: ignoreList },
-      (err, result) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      }
-    );
-  });
-};
+const TransmuteIpfs = require("./");
 
 const tests = async () => {
-  let gitignore = await readFile(".gitignore");
-  let npmignore = await readFile(".npmignore");
-  let ignoreList = [gitignore, npmignore]
-    .map(file => file.split("\n"))
-    .reduce((acc, cur) => acc.concat(cur));
-  // console.log(ignoreList);
+  let TI = new TransmuteIpfs();
+  let ignoreList = await TI.getIgnoreList();
   let testDataDir = path.join(__dirname, "./test_data");
-  let results = await addDir(testDataDir, ignoreList);
+  let results = await TI.addDirectory(testDataDir, ignoreList);
   let dappHash = results.pop().hash;
   console.log("http://localhost:8080/ipfs/" + dappHash);
 };
