1bbe95a1e0374e1ca4ec50527696568e344db1a5
Orie Steele <orie@ohs.io>
2018-01-12 17:39:34 -0600

    refactor transmute ipfs

--- packages/transmute-ipfs/index.js
+++ packages/transmute-ipfs/index.js
@@ -1,19 +1,43 @@
+var fs = require("fs");
 var ipfsAPI = require("ipfs-api");
 
-// connect to ipfs daemon API server
-var ipfs = ipfsAPI("localhost", "5001", { protocol: "http" });
-// leaving out the arguments will default to these values
+const readFile = filePath => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", function(err, contents) {
+      if (err) {
+        reject(err);
+      }
+      resolve(contents);
+    });
+  });
+};
 
-module.exports = {
-  stat: ipfs.repo.stat,
-  addDirectory: directoryPath => {
+module.exports = class TransmuteIpfs {
+  constructor(host = "localhost", port = "5001", protocol = "http") {
+    this.ipfs = ipfsAPI(host, port, { protocol: protocol });
+  }
+
+  async getIgnoreList() {
+    const gitignore = await readFile(".gitignore");
+    const npmignore = await readFile(".npmignore");
+    const ignoreList = [gitignore, npmignore]
+      .map(file => file.split("\n"))
+      .reduce((acc, cur) => acc.concat(cur));
+    return ignoreList;
+  }
+
+  async addDirectory(directoryPath, ignoreList) {
     return new Promise((resolve, reject) => {
-      ipfs.util.addFromFs(directoryPath, (err, files) => {
-        if (err) {
-          reject(err);
+      this.ipfs.util.addFromFs(
+        directoryPath,
+        { recursive: true, ignore: ignoreList },
+        (err, result) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(result);
         }
-        resolve(files);
-      });
+      );
     });
   }
 };
