6f13ecd76e9fe5e54b445eccbaa625d215a9681f
Orie Steele <orie@ohs.io>
2018-01-13 14:51:23 -0600

    refactor relic to use provider engine

--- packages/transmute-framework/src/web3.ts
+++ packages/transmute-framework/src/web3.ts
@@ -1,36 +1,30 @@
-import { W3 } from './transmute-framework'
+import { W3 } from "./transmute-framework";
+import Web3 from "web3";
 
-let Web3 = require('web3')
-
-declare var web3: any
+const ProviderEngine = require("web3-provider-engine");
+const RpcSubprovider = require("web3-provider-engine/subproviders/rpc.js");
+const WalletSubprovider = require("web3-provider-engine/subproviders/wallet.js");
 
 export interface ITransmuteWeb3Config {
-  providerUrl: string
-}
-
-// "web3": "^0.20.2"
-export const getStable = (config: ITransmuteWeb3Config) => {
-  // let provider: any = new Web3.providers.HttpProvider(config.providerUrl);
-
-  // let w3 = new W3(provider);
-  // // w3.defaultAccount = testAccounts[0];
-  // W3.Default = w3
-  // return w3
-
-  return new W3()
-}
-
-// "web3": "^1.0.0-beta.27"
-export const getUnstable = (config: ITransmuteWeb3Config) => {
-  if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider)
-  } else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider(config.providerUrl))
-  }
-  return web3
+  providerUrl: string;
+  wallet?: any;
 }
 
 export const transmuteWeb3 = (config: ITransmuteWeb3Config, experimental = false) => {
-  return experimental ? getUnstable(config) : getStable(config)
-}
+  const engine = new ProviderEngine();
+
+  if (config.wallet) {
+    engine.addProvider(new WalletSubprovider(config.wallet, {}));
+  }
+
+  engine.addProvider(
+    new RpcSubprovider({
+      rpcUrl: config.providerUrl
+    })
+  );
+
+  const web3 = new Web3(engine);
+  engine.start();
+
+  return web3;
+};
