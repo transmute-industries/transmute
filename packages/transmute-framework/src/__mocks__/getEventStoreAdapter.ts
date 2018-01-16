7d1daa9e2833c768c95be4b56aee3ad6dfaa019f
Orie Steele <orie@ohs.io>
2018-01-15 18:34:15 -0600

    refactor

---
+++ packages/transmute-framework/src/__mocks__/getEventStoreAdapter.ts
@@ -0,0 +1,48 @@
+import { getDefaultRelic } from './getRelic'
+
+import {
+  W3,
+  Relic,
+  TransmuteContracts,
+  EventStoreAdapter,
+  Factory,
+  Store,
+  EventStore,
+  EventStoreFactory
+} from '../transmute-framework'
+
+const bs58 = require('bs58')
+const util = require('ethereumjs-util')
+
+let ipfsAdapter = require('../../../transmute-adapter-ipfs')
+let nodeStorageAdapter = require('../../../transmute-adapter-node-storage')
+
+let nodeStorageDB = nodeStorageAdapter.getStorage()
+let ipfs = ipfsAdapter.getStorage()
+
+export const getDefaultEventStoreAdapter = () => {
+  return new EventStoreAdapter({
+    I: {
+      keyName: 'multihash',
+      adapter: ipfsAdapter,
+      db: ipfs,
+      readIDFromBytes32: (bytes32: string) => {
+        return bs58.encode(new Buffer('1220' + bytes32.slice(2), 'hex'))
+      },
+      writeIDToBytes32: (id: string) => {
+        return '0x' + new Buffer(bs58.decode(id).slice(2)).toString('hex')
+      }
+    },
+    N: {
+      keyName: 'sha1',
+      adapter: nodeStorageAdapter,
+      db: nodeStorageDB,
+      readIDFromBytes32: (bytes32: string) => {
+        return util.toAscii(bytes32).replace(/\u0000/g, '')
+      },
+      writeIDToBytes32: (id: string) => {
+        return id
+      }
+    }
+  })
+}
