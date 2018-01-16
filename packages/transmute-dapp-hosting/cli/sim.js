0cbbec66f1726ca393d689d160a1a2f7a5b7932c
Orie Steele <orie@ohs.io>
2018-01-15 19:23:12 -0600

    add example

--- packages/transmute-dapp-hosting/cli/sim.js
+++ packages/transmute-dapp-hosting/cli/sim.js
@@ -1,89 +1,17 @@
-const path = require("path");
-const fse = require("fs-extra");
-const fs = require("fs");
-const shell = require("shelljs");
-const T = require("transmute-framework");
-const TransmuteCrypto = require("transmute-crypto");
-const transmute = require("../transmute");
+const { init } = require("../transmute");
 
-const generateTestWallets = async num => {
-  const sodium = await TransmuteCrypto.getSodium();
-  let testWallets = [];
-  for (let i = 0; i < num; i++) {
-    const alice = sodium.crypto_box_keypair();
-    const unPrefixedPrivateKeyHexString = sodium.to_hex(alice.privateKey);
-    let address = T.Utils.privateKeyHexToAddress(
-      "0x" + unPrefixedPrivateKeyHexString
-    );
-    testWallets.push({
-      address: "0x" + sodium.to_hex(address),
-      privateKey: "0x" + unPrefixedPrivateKeyHexString
-    });
-  }
-  return testWallets;
-};
+(async () => {
+  const { T, relic, accounts, eventStoreAdapter, readModelAdapter } = await init();
+  // console.log(accounts);
 
-module.exports = vorpal => {
-  vorpal.command("sim", "simulate use").action(async (args, callback) => {
-    vorpal.logger.log("\n" + "Simulating...\n");
+  let factory = await T.Factory.create(relic.web3, accounts[0]);
+  let store = await T.Factory.createStore(
+    factory,
+    accounts,
+    eventStoreAdapter,
+    relic.web3,
+    accounts[0]
+  );
 
-    let subscriptions = {
-      T1: {
-        PACKAGE_LIMIT: 3,
-        SPACE_LIMIT: 1 // KB, MB, GB
-      }
-    };
-
-    let activeSubscriptions = {};
-
-    // - System generates Customer wallets for test.
-    let wallets = await generateTestWallets(3);
-    vorpal.logger.info("Customers already have accounts.");
-    vorpal.logger.log("Alice:\t" + wallets[0].address);
-    vorpal.logger.log("Bob:\t" + wallets[1].address);
-    vorpal.logger.log("Eve:\t" + wallets[2].address);
-    vorpal.logger.log("\n");
-
-    // - User purchases a subscription with Stripe.
-    vorpal.logger.info("Alice has T1 Subscription.");
-    activeSubscriptions[wallets[0].address] = subscriptions.T1;
-    vorpal.logger.log(activeSubscriptions);
-   
-    // - System uses factory to create a store for the customer
-    let {
-      relic,
-      accounts,
-      // eventStoreAdapter,
-      // readModelAdapter
-    } = await transmute();
-    
-    console.log(accounts)
-
-    // let factory = await T.Factory.create(relic.web3, accounts[0]);
-    // let factorReadModel = await T.Factory.getReadModel(
-    //   factory,
-    //   eventStoreAdapter,
-    //   readModelAdapter,
-    //   relic.web3,
-    //   accounts[0]
-    // );
-    // let eventStore = await T.Factory.createStore(
-    //   factory,
-    //   accounts,
-    //   eventStoreAdapter,
-    //   relic.web3,
-    //   accounts[0]
-    // );
-    // let storeOwner = await eventStore.owner()
-
-    vorpal.logger.info("Alice has EventStore");
-
-    // - System monitors packages deployed and utilization.
-    // - User can issue delete packages to clean up space.
-
-    vorpal.logger.log("\n");
-    callback();
-  });
-
-  return vorpal;
-};
+  process.exit(0);
+})();
