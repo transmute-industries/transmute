3f7f234b7a59fa103dc0852fda43149d2150b6c1
Orie Steele <orie@ohs.io>
2018-01-18 19:11:13 -0600

    resolve vorpal module loading async issue

---
+++ packages/transmute-dapp-hosting/cli/init.js
@@ -0,0 +1,34 @@
+
+// module.exports = vorpal => {
+//   vorpal
+//     .command("init", "Setup a CLI account.")
+//     .action((args, callback) => {
+//       // console.log(callback(web3.eth.getAccounts()))
+
+//       // console.log(accounts)
+//       // let relic = new T.Relic(web3)
+//       //   let accounts = await relic.getAccounts();
+//       //   console.log(accounts)
+
+//       //   console.log(wallet)
+
+//       //   const T = require("transmute-framework");
+//       //   const transmute = require("../transmute");
+
+//       //   const {
+//       //     T,
+//       //     relic,
+//       //     // accounts,
+//       //     // eventStoreAdapter,
+//       //     // readModelAdapter,
+//       //     // getRelicWalletWithPrivateKey
+//       //   } = await transmute.init();
+
+//       //   console.log(accounts);
+
+
+//     //   console.log("working");
+//     });
+
+//   return vorpal;
+// };
