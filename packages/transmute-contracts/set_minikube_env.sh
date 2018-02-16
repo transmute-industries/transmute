c519a368fddb9d3297e6536c51a3f4ab854b0797
Orie Steele <orie@ohs.io>
2018-02-16 10:14:05 -0600

    clean up for testnet deploy

--- packages/transmute-contracts/set_minikube_env.sh
+++
@@ -1,5 +0,0 @@
-
-
-export GANACHE_CLI=$(minikube --namespace transmute-testrpc  service transmute-testrpc-ganache-cli --url )
-export IPFS_GATEWAY=$(minikube service --url ipfs-gateway --namespace transmute-ipfs)
-export IPFS_API=$(minikube service --url ipfs-api --namespace transmute-ipfs)
--- packages/transmute-contracts/truffle.js
+++ packages/transmute-contracts/truffle.js
@@ -11,18 +11,18 @@ module.exports = {
       port: PORT,
       network_id: "*",
       gas: 4600000
-    }
-    // "ropsten": {
-    //   provider: new LightWalletProvider({
-    //     keystore: './sigmate-v3-ti.json',
-    //     password: global.lightWalletPassword,
-    //     rpcUrl: 'https://ropsten.infura.io',
-    //     debug: true, // optional, show JSON-RPC logs
-    //     // prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this of wei
-    //     pollingInterval: 2000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
-    //   }),
-    //   network_id: '*',
-    // },
+    },
+    "ropsten": {
+      provider: new LightWalletProvider({
+        keystore: './sigmate-v3-ti.json',
+        password: process.env.PASSWORD,
+        rpcUrl: 'https://ropsten.infura.io',
+        debug: true, // optional, show JSON-RPC logs
+        // prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this of wei
+        pollingInterval: 2000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '*',
+    },
     // "parity": {
     //   network_id: "*",
     //   host: "localhost",
