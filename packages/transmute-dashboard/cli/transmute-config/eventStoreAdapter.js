7300a01c1be44103d06e3409c520ab5b700d248f
Orie Steele <orie@ohs.io>
2018-02-23 23:40:34 -0600

    updates

--- packages/transmute-dashboard/cli/transmute-config/eventStoreAdapter.js
+++ packages/transmute-dashboard/cli/transmute-config/eventStoreAdapter.js
@@ -4,9 +4,43 @@ const bs58 = require('bs58');
 
 const axios = require('axios');
 
-let ipfsAdapter = require('transmute-adapter-ipfs');
 
 const { ipfsConfig } = require('./ipfs');
+
+const IPFS = require('ipfs-mini');
+
+const getStorage = config => {
+  if (!config) {
+    throw new Error(
+      'Config required, see https://github.com/SilentCicero/ipfs-mini'
+    );
+  }
+  return new IPFS(config);
+};
+
+const getItem = (ipfs, key) => {
+  return new Promise((resolve, reject) => {
+    ipfs.catJSON(key, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+};
+
+const setItem = (ipfs, value) => {
+  return new Promise((resolve, reject) => {
+    ipfs.addJSON(value, (err, result) => {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+};
+
+const ipfsAdapter = {
+  getStorage,
+  getItem,
+  setItem
+};
 
 const getEventStoreAdapterAsync = async () => {
   const ipfs = ipfsAdapter.getStorage({
--- packages/transmute-dashboard/cli/transmute-config/readModelAdapter.js
+++ packages/transmute-dashboard/cli/transmute-config/readModelAdapter.js
@@ -1,4 +1,27 @@
-let localStorageAdapter = require('transmute-adapter-local-storage');
+var sha1 = require('js-sha1');
+
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./local-storage');
+
+const localStorageAdapter = {
+  getStorage: function() {
+    return localStorage;
+  },
+  getItem: function(db, key) {
+    return new Promise(function(resolve, reject) {
+      resolve(JSON.parse(db.getItem(key)));
+    });
+  },
+  setItem: function(db, value) {
+    const valueAsJsonString = JSON.stringify(value);
+    const key = sha1(valueAsJsonString).substring(0, 32); // not safe... consider guids here...
+    return new Promise(function(resolve, reject) {
+      db.setItem(key, valueAsJsonString);
+      resolve(key);
+    });
+  }
+};
+
 let localStorageDB = localStorageAdapter.getStorage();
 
 const getReadModelAdapterAsync = async () => {
