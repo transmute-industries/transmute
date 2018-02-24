e614e487f7769e02d931af9e78c98b3805b14bbf
Orie Steele <orie@ohs.io>
2018-02-24 00:09:50 -0600

    fix route in case of complex path

--- packages/transmute-dashboard/src/Components/Routes/index.js
+++ packages/transmute-dashboard/src/Components/Routes/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import { Route } from 'react-router';
+// import { Route } from 'react-router';
 
 // import Home from '../Home';
 import Status from '../Status';
+
 
 class Routes extends React.Component {
   render() {
     return (
       <div>
-        <Route exact={true} path="/" component={Status} />
+
+        <Status />
+        {/* <Route exact={true} path="/" component={Status} /> */}
         {/* <Route exact={true} path="/status" component={Status} /> */}
       </div>
     );
