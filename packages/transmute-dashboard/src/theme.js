6de731faeb493a0b2f0984ec1e2da87e842d65e5
Orie Steele <orie@ohs.io>
2018-02-23 21:44:01 -0600

    colors

--- packages/transmute-dashboard/src/theme.js
+++ packages/transmute-dashboard/src/theme.js
@@ -1,19 +1,17 @@
-import { createMuiTheme } from 'material-ui/styles';
-import {
-  cyan,
-  pink,
-  red
-} from 'material-ui/colors';
+// https://material.io/color/#!/?view.left=0&view.right=0&secondary.color=651FFF&primary.color=006064
 
+import { createMuiTheme } from 'material-ui/styles';
+import { teal, indigo, red } from 'material-ui/colors';
 import spacing from 'material-ui/styles/spacing';
+
 
 // import { fade } from 'material-ui/styles/utils/colorManipulator';
 
 export default createMuiTheme({
   spacing: spacing,
   palette: {
-    primary: cyan,
-    secondary: pink,
+    primary: teal,
+    secondary: indigo,
     error: red
     // Used by `getContrastText()` to maximize the contrast between the background and
     // the text.
