fcbf84a91f5f3c3a4dc71d14e1dac2d4cb9df580
Orie Steele <orie@ohs.io>
2018-02-23 22:03:55 -0600

    working prod builds unminified

--- packages/transmute-dashboard/src/store/transmute/transmute-config/readModelAdapter.js
+++ packages/transmute-dashboard/src/store/transmute/transmute-config/readModelAdapter.js
@@ -1,4 +1,31 @@
-import localStorageAdapter from 'transmute-adapter-local-storage';
+var sha1 = require('js-sha1');
+
+var localStorage = window ? window.localStorage : null;
+
+if (!window) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+const localStorageAdapter = {
+  getStorage: function() {
+    return localStorage;
+  },
+  getItem: function(db, key) {
+    return new Promise(function(resolve, reject) {
+      resolve(JSON.parse(db.getItem(key)));
+    });
+  },
+  setItem: function(db, value) {
+    const valueAsJsonString = JSON.stringify(value);
+    const key = sha1(valueAsJsonString).substring(0, 32); // not safe... consider guids here...
+    return new Promise(function(resolve, reject) {
+      db.setItem(key, valueAsJsonString);
+      resolve(key);
+    });
+  }
+};
+
 const localStorageDB = localStorageAdapter.getStorage();
 
 const getReadModelAdapterAsync = async () => {
