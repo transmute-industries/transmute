3ff3c1d185d64fdcb6e40a726bf3645e7c0b30c0
Orie Steele <orie@ohs.io>
2018-03-03 20:26:42 -0600

    test

--- packages/transmute-minikube/src/_root.test.js
+++ packages/transmute-minikube/src/_root.test.js
@@ -1,3 +1,14 @@
-it('root test', () => {
-  console.log('root test...');
+// https://emojipedia.org/unicode-8.0/
+const shell = require('shelljs');
+
+it('can get services', () => {
+  if (shell.exec('helm install stable/ipfs').code !== 0) {
+    shell.echo('🙍  Error failed to install IPFS with helm.');
+    shell.exit(1);
+  }
+
+  if (shell.exec('helm ls').code !== 0) {
+    shell.echo('🙍  Error failed list helm charts');
+    shell.exit(1);
+  }
 });
