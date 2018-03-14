05fd482aefd6a06f23434fc3f3a5b2a0db85481a
Orie Steele <orie@ohs.io>
2018-03-13 21:46:51 -0500

    tweak

--- travis.sh
+++ travis.sh
@@ -4,10 +4,14 @@ helm ls
 
 kubectl get services
 
+export KONG_PROXY_PORT=$(kubectl get service mini-kong-kong-proxy -o json | jq -r '.spec.ports[0].nodePort')
+
+export KONG_PROXY_URL=$(echo 'https://transmute.minikube:'$KONG_PROXY_PORT)
+
 export IPFS_CLUSTER_IP=$(kubectl get service mini-ipfs-ipfs -o json | jq -r '.spec.clusterIP');
 
 curl -k -X POST \
-  --url https://transmute.minikube:32444/apis/ \
+  --url $KONG_PROXY_URL/apis/ \
   --data 'name=ipfs' \
   --data 'hosts=ipfs.transmute.minikube' \
   --data 'upstream_url=http://'$IPFS_CLUSTER_IP':5001/'
@@ -18,7 +22,7 @@ curl -k https://ipfs.transmute.minikube:32443/api/v0/id
 export GANACHE_CLUSTER_IP=$(kubectl get service mini-ganache-ganache-cli -o json | jq -r '.spec.clusterIP');
 
 curl -k -X POST \
-  --url https://transmute.minikube:32444/apis/ \
+  --url $KONG_PROXY_URL/apis/ \
   --data 'name=ganache' \
   --data 'hosts=ganache.transmute.minikube' \
   --data 'upstream_url=http://'$GANACHE_CLUSTER_IP':8545/'
