31a8dbecc7e37a80cbc33c5f1d6062dc01eb3ff5
Orie Steele <orie@ohs.io>
2018-03-24 11:57:48 -0500

    test

--- packages/transmute-eventstore/truffle-config.js
+++ packages/transmute-eventstore/truffle-config.js
@@ -1,21 +1,23 @@
+const Web3 = require('web3');
 
-
-const transmuteConfig = require('../../transmute-config')
+const transmuteConfig = require('../../transmute-config');
 
 const TRANSMUTE_ENV = process.env.TRANSMUTE_ENV;
 
 module.exports = {
-  migrations_directory: "./migrations",
+  migrations_directory: './migrations',
   networks: {
     development: {
       host: transmuteConfig.env[TRANSMUTE_ENV].web3Config.host,
       port: transmuteConfig.env[TRANSMUTE_ENV].web3Config.port,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
     },
     minikube: {
-      host: transmuteConfig.env[TRANSMUTE_ENV].web3Config.host,
-      port: transmuteConfig.env[TRANSMUTE_ENV].web3Config.port,
-      network_id: "*" // Match any network id
+      provider: new Web3.providers.HttpProvider(
+        transmuteConfig.env[TRANSMUTE_ENV].web3Config.providerUrl
+      ),
+      network_id: '*',
+      gas: 4600000
     }
   }
 };
