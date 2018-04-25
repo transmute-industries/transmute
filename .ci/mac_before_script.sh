6a9f5ccb65dadf70debf3f6d47ae8cfb2c05dd51
Josh Cox <josh@transmute.network>
2018-04-25 11:17:27 -0500

    Adding rvm keys

--- .ci/mac_before_script.sh
+++ .ci/mac_before_script.sh
@@ -4,4 +4,5 @@ mkdir -p ~/.gnupg
 touch ~/.gnupg/gpg.conf
 sudo chown travis ~/.gnupg/gpg.conf
 chmod 0600 ~/.gnupg/gpg.conf
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 rvm get stable
