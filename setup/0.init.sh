0d6018e69ad153824019f64f867e753f048c8cb6
Josh Cox <josh@transmute.network>
2018-04-26 13:41:29 -0500

    Festival priority

--- setup/0.init.sh
+++ setup/0.init.sh
@@ -9,12 +9,12 @@ speaker () {
   if [ "$USE_VOX" = y ]; then
     if type "say"        > /dev/null 2> /dev/null; then
       say "$WELCOME"
+    elif type "festival" > /dev/null 2> /dev/null; then
+      echo "$WELCOME" | festival --tts > /dev/null 2> /dev/null
     elif type "espeak"   > /dev/null 2> /dev/null; then
       espeak "$WELCOME"  > /dev/null 2> /dev/null
     elif type "spd-say"  > /dev/null 2> /dev/null; then
       spd-say "$WELCOME" > /dev/null 2> /dev/null
-    elif type "festival" > /dev/null 2> /dev/null; then
-      echo "$WELCOME" | festival --tts > /dev/null 2> /dev/null
     fi
   fi
 }
