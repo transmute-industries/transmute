3e6d941f336323476ea0045b61026ba0c89065e8
Josh Cox <josh@transmute.network>
2018-04-27 13:16:12 -0500

    Adding lerna and truffle to bootstrap

---
+++ .ci/linux_before_install.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+export CWD=$(pwd)
+
+chkdir () {
+  if [ ! -w $1 ] ; then
+    sudo mkdir -p $1
+    sudo chown $USER. $1
+  fi
+  if [ ! -w $1 ] ; then
+    errror
+    echo "Cannot write to $1, please check your permissions"
+    exit 2
+  fi
+}
+
+chkdir /etc/kubernetes
+chkdir /etc/systemd
+chkdir /var/lib
+chkdir /var/lib/localkube
+chkdir /var/lib/localkube/certs
+chkdir /lib/systemd/system
+chkdir /usr/bin
+chkdir /usr/local/bin
+
+sudo apt-get install -yqq \
+  autopoint debhelper dh-apparmor intltool-debian jq libmail-sendmail-perl \
+  libselinux1-dev libsepol1-dev libslang2-dev libsys-hostname-long-perl \
+  jq build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev \
+  libselinux1-dev debhelper lsb-release pkg-config po-debconf \
+  autoconf automake autopoint libtool python2.7-dev
+
+#curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+#curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+if [ ! -e "/usr/local/bin/nsenter" ]; then
+  TMP=$(mktemp -d)
+  cd $TMP
+  wget https://www.kernel.org/pub/linux/utils/util-linux/v2.25/util-linux-2.25.tar.gz
+  tar -xvf util-linux-2.25.tar.gz
+  cd util-linux-2.25
+  ./configure
+  make nsenter
+  sudo cp nsenter /usr/local/bin
+  cd $CWD
+  rm -Rf $TMP
+fi
+
+if [ ! -e "$HOME/.transmute/bin/minikube" ]; then
+  TMP=$(mktemp -d)
+  cd $TMP
+  mkdir -p "$HOME/.transmute/bin"
+  curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube $HOME/.transmute/bin/
+  cd $CWD
+  rm -Rf $TMP
+fi
