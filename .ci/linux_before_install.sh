2f9babfe4516ef9664c937f1dd3c96e23ebb560e
Josh Cox <josh@transmute.network>
2018-04-27 22:16:50 -0500

    before

--- .ci/linux_before_install.sh
+++ .ci/linux_before_install.sh
@@ -29,28 +29,3 @@ sudo apt-get install -yqq \
   jq build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev \
   libselinux1-dev debhelper lsb-release pkg-config po-debconf \
   autoconf automake autopoint libtool python2.7-dev
-
-#curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-#curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-
-if [ ! -e "/usr/local/bin/nsenter" ]; then
-  TMP=$(mktemp -d)
-  cd $TMP
-  wget https://www.kernel.org/pub/linux/utils/util-linux/v2.25/util-linux-2.25.tar.gz
-  tar -xvf util-linux-2.25.tar.gz
-  cd util-linux-2.25
-  ./configure
-  make nsenter
-  sudo cp nsenter /usr/local/bin
-  cd $CWD
-  rm -Rf $TMP
-fi
-
-if [ ! -e "$HOME/.transmute/bin/minikube" ]; then
-  TMP=$(mktemp -d)
-  cd $TMP
-  mkdir -p "$HOME/.transmute/bin"
-  curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube $HOME/.transmute/bin/
-  cd $CWD
-  rm -Rf $TMP
-fi
