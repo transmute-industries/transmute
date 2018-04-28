a95fc54f313e37a9eb111947d17e31e82bbd8f71
Josh Cox <josh@transmute.network>
2018-04-28 09:57:58 -0500

    nsenter

--- .ci/ubuntu-compile-nsenter.sh
+++ .ci/ubuntu-compile-nsenter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# Copyright 2017-2018 The Jaeger Authors
+# Copyright 2017 The Jaeger Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -17,8 +17,15 @@ set -e
 
 sudo apt-get update
 sudo apt-get install libncurses5-dev libslang2-dev gettext zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config po-debconf autoconf automake autopoint libtool
-mkdir -p $HOME/nsenter
-if [ ! -d $HOME/nsenster/util-linux-2.30.2 ]; then
-  wget https://www.kernel.org/pub/linux/utils/util-linux/v2.30/util-linux-2.30.2.tar.gz -qO - | tar -xz -C $HOME/nsenter/
-  cd $HOME/nsenter/util-linux-2.30.2 && ./autogen.sh && ./configure && make nsenter
-fi
+TMP=$(mktemp -d)
+cleanup_tmp () {
+  rm -Rf $TMP
+}
+trap cleanup_tmp EXIT
+cd $TMP
+
+wget https://www.kernel.org/pub/linux/utils/util-linux/v2.30/util-linux-2.30.2.tar.gz -qO - | tar -xz -C $TMP/
+cd $TMP/util-linux-2.30.2 && ./autogen.sh && ./configure && make nsenter
+sudo cp $TMP/util-linux-2.30.2/nsenter $HOME/.local/bin/nsenter
+sudo chmod +x $HOME/.local/bin/nsenter
+sudo cp $HOME/.local/bin/nsenter /usr/bin/nsenter
