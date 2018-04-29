7dbe456c7a3f1492a00d9c5641ddb2e11560ff34
Josh Cox <josh@transmute.network>
2018-04-29 08:32:21 -0500

    verbosity +1

--- scripts/configure-kong-okta-ipfs.sh
+++ scripts/configure-kong-okta-ipfs.sh
@@ -9,23 +9,28 @@ echo 'Configure Kong to use Okta to secure IPFS'
 curl -k -X POST $KONG_ADMIN_URL/apis/ipfs/plugins \
     --data "name=jwt"
 
+echo 'Export CONSUMER_ID'
 export CONSUMER_ID=$(curl -k -X POST $KONG_ADMIN_URL/consumers \
     --data "username=bob@example.com" \
     --data "custom_id=0" \
     | jq -r '.id')
 
 # Download JWT Signing Key
+echo  'Download JWT Signing Key'
 node ./scripts/okta/write-okta-pem.js
 
 # Connect the API Consumer to okta
+echo 'Connect the API Consumer to okta'
 curl -k -X POST $KONG_ADMIN_URL/consumers/$CONSUMER_ID/jwt \
     -F "algorithm=RS256" \
     -F "rsa_public_key=@./scripts/okta/okta.pem" \
     -F "key=https://"$OKTA_HOSTNAME"/oauth2/default"
 
 # Get an okta jwt
+echo 'Get an okta jwt'
 export ACCESS_TOKEN=$(node ./scripts/okta/get-okta-token.js)
 
+echo 'Get api v0 id'
 curl -k -X GET \
     --url 'https://ipfs.transmute.minikube:'$KONG_PROXY_PORT/api/v0/id \
     --header 'Authorization: Bearer '$ACCESS_TOKEN
