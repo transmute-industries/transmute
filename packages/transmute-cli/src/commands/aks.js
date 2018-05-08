8bf3cab1fbd2af77d1fe18886e0ddd1a6cc99094
Josh Cox <josh@transmute.network>
2018-05-08 00:34:57 -0500

    Add provision and register methods to aks

--- packages/transmute-cli/src/commands/aks.js
+++ packages/transmute-cli/src/commands/aks.js
@@ -8,3 +8,13 @@ export function ls() {
   var ls_cmd = 'az aks list';
   shell.exec(ls_cmd);
 }
+
+export function register() {
+  var reg_cmd = 'az provider register -n Microsoft.ContainerService';
+  shell.exec(reg_cmd);
+}
+
+export function provision() {
+  var prov_cmd = 'az aks create --resource-group myResourceGroup --name myAKSCluster --node-count 1 --generate-ssh-keys';
+  shell.exec(prov_cmd);
+}
