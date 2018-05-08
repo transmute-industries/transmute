6a8c50999da2e33dd2eff6ad2730c63133eeea6c
Orie Steele <orie@transmute.industries>
2018-05-08 16:25:43 -0500

    remigrate scripts for new cluster

--- packages/transmute-cli/src/okta/login.js
+++ packages/transmute-cli/src/okta/login.js
@@ -28,10 +28,13 @@ module.exports = async () => {
   var code_challenge = base64URLEncode(sha256(code_verifier));
 
   const url = `https://${okta_host}/oauth2/default/v1/authorize?client_id=${client_id}&response_type=code&scope=openid&redirect_uri=${redirect_uri}&state=state-8600b31f-52d1-4dca-987c-386e3d8967e9&code_challenge_method=S256&code_challenge=${code_challenge}`;
-  const command = `open "${url}"`;
-  const execution = shell.exec(command);
-  if (execution.code !== 0) {
-    shell.exit(1);
-  }
+
+  console.log('open this url: ');
+  console.log(url);
+  // const command = `open "${url}"`;
+  // const execution = shell.exec(command);
+  // if (execution.code !== 0) {
+  //   shell.exit(1);
+  // }
   return callbackServer(code_verifier);
 };
