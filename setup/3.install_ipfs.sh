6092ece3157e261c31abb313a93b3943a4094dfe
Josh Cox <josh@transmute.network>
2018-05-10 17:37:28 -0500

    Add openebs to setup scripts

--- setup/3.install_ipfs.sh
+++ setup/3.install_ipfs.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+kubectl create -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/openebs-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/openebs/openebs/master/k8s/openebs-storageclasses.yaml
+kubectl create -f ./components/ipfs/openebs-ipfs.yaml
+hthhps://raw.githubusercontent.com/openebs/openebs/master/k8s/openebs-storageclasses.yaml
+
+# while loop
+countone=1
+# timeout for 15 minutes
+while [ $countone -lt 151 ]
+do
+  echo -n '.'
+  RESULT=$(kubectl get po --namespace=kube-system | grep openebs-provisioner | grep Running)
+  if [ "$RESULT" ]; then
+      echo '.'
+      echo "$RESULT"
+      break
+  fi
+  countone=`expr $countone + 1`
+  sleep 3
+done
+
+echo "openebs is now up and running"
+
+
 helm install stable/ipfs --name decentralized-storage
 
 export IPFS_CLUSTER_IP=$(kubectl get service decentralized-storage-ipfs -o json | jq -r '.spec.clusterIP');
