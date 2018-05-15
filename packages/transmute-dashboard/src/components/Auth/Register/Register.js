4d64ad5c22e28a0b3ccc79425276aee60dca03e7
Eric Olszewski <eolszewski@gmail.com>
2018-05-15 09:03:12 -0500

    Add basic client-side validation for algos of uploaded keys.

--- packages/transmute-dashboard/src/components/Auth/Register/Register.js
+++ packages/transmute-dashboard/src/components/Auth/Register/Register.js
@@ -35,8 +35,10 @@ const styles = theme => ({
 class Register extends Component {
   state = {
     error: null,
-    edArmorPub: null,
-    secArmorPub: null
+    ed25519: null,
+    ed25519_filename: null,
+    secp256k1: null,
+    secp256k1_filename: null
   };
 
   handleSubmit = e => {
@@ -50,15 +52,24 @@ class Register extends Component {
   handleKeyUpload = key => event => {
     const file = event.target.files[0];
     const filename = event.target.value.split(/(\\|\/)/g).pop();
+    const key_filename = `${key}_filename`;
 
     let reader = new window.FileReader();
     reader.onload = () => {
       const pub = openpgp.key.readArmored(
         reader.result
       ).keys[0];
-      this.setState({
-        [key]: pub.armor()
-      });
+      if (pub.primaryKey.params[0].getName() !== key) {
+        this.setState({
+          error: `Uploaded key is ${pub.primaryKey.params[0].getName()}, please upload a ${key} key`
+        });
+      } else {
+        this.setState({
+          error: null,
+          [key]: pub.armor(),
+          [key_filename]: filename
+        });
+      }
     };
     reader.readAsText(file);
   };
@@ -70,7 +81,8 @@ class Register extends Component {
   };
 
   render() {
-    const { classes, user, edArmorPub, secArmorPub } = this.props;
+    const { classes, user } = this.props;
+    const { ed25519, ed25519_filename, secp256k1, secp256k1_filename, error } = this.state;
     const registerForm = (
       <Grid container className={classNames(classes.root)}>
         <Grid item xs={12}>
@@ -88,7 +100,7 @@ class Register extends Component {
             <input
               id="edKeyFile"
               type="file"
-              onChange={this.handleKeyUpload('edArmorPub')}
+              onChange={this.handleKeyUpload('ed25519').bind(this)}
               style={{
                 width: 0,
                 height: 0,
@@ -102,9 +114,8 @@ class Register extends Component {
               color="secondary"
               component="label"
               htmlFor="edKeyFile"
-              // disabled={edArmorPub !== null}
             >
-              Upload ED25519 Armored Public Key
+              {ed25519 == null ? 'Upload ED25519 Armored Public Key' : ed25519_filename}
             </Button>
           </FormControl>
         </Grid>
@@ -116,7 +127,7 @@ class Register extends Component {
             <input
               id="secKeyFile"
               type="file"
-              onChange={this.handleKeyUpload('secArmorPub')}
+              onChange={this.handleKeyUpload('secp256k1').bind(this)}
               style={{
                 width: 0,
                 height: 0,
@@ -130,9 +141,8 @@ class Register extends Component {
               color="secondary"
               component="label"
               htmlFor="secKeyFile"
-              // disabled={secArmorPub !== null}
             >
-              Upload SECP256K1 Armored Public Key
+              {secp256k1 == null ? 'Upload SECP256K1 Armored Public Key' : secp256k1_filename}
             </Button>
           </FormControl>
         </Grid>
@@ -142,14 +152,20 @@ class Register extends Component {
             variant="raised"
             color="secondary"
             disabled={
-              this.state.edArmorPub == null ||
-              this.state.secArmorPub == null
+              this.state.ed25519 == null ||
+              this.state.secp256k1 == null
             }
             onClick={this.handleSubmit}
           >
             Register
           </Button>
         </Grid>
+
+        {error !== null && <Grid item xs={12}>
+          <p>
+            {error}
+          </p>
+        </Grid>}
       </Grid>
     );
 
@@ -177,10 +193,10 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     registerWithActivationEmail: async formData => {
-      const { edArmorPub, secArmorPub } = formData;
+      const { ed25519, secp256k1 } = formData;
       const action = await actions.register({
-        edArmorPub,
-        secArmorPub
+        ed25519,
+        secp256k1
       });
       dispatch(action);
     }
