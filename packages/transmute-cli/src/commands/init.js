4c6a54fe06bb06c54142379d0c04fea603bd85ec
Josh Cox <josh@webhosting.coop>
2018-05-17 15:15:01 -0500

    cleanup

--- packages/transmute-cli/src/commands/init.js
+++ packages/transmute-cli/src/commands/init.js
@@ -3,9 +3,5 @@ const run = require('./runner');
 export function minikube( clusterName ) {
   let prov_cmd = "$HOME/.transmute/git/transmute/scripts/initializer " + clusterName;
   run.ner( prov_cmd );
-  let cmd2run =  '/bin/bash';
-  let arg1 =  "-c";
-  let arg2 =  "$HOME/.transmute/git/transmute/setup/0.init.sh";
-  //run.spawn( cmd2run, arg1, arg2 );
 }
 
