c288876d8784fe2f1c09eff6f3d2c97e2bcacad6
Josh Cox <josh@webhosting.coop>
2018-05-17 19:11:39 -0500

    lsremoved

--- packages/transmute-cli/src/commands/init.js
+++ packages/transmute-cli/src/commands/init.js
@@ -1,8 +1,6 @@
 const run = require('./runner');
 
 export function minikube( clusterName ) {
-  let prov_cmd = "ls /root";
-  run.ner( prov_cmd );
   prov_cmd = "$HOME/.transmute/git/transmute/scripts/initializer " + clusterName;
   run.ner( prov_cmd );
 }
