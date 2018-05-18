68ab1f386abce2cde677ac51d8308e35dbe1ece1
Josh Cox <josh@webhosting.coop>
2018-05-17 19:00:47 -0500

    moved kong env vars to after kong has been installed

--- packages/transmute-cli/src/commands/init.js
+++ packages/transmute-cli/src/commands/init.js
@@ -1,7 +1,9 @@
 const run = require('./runner');
 
 export function minikube( clusterName ) {
-  let prov_cmd = "$HOME/.transmute/git/transmute/scripts/initializer " + clusterName;
+  let prov_cmd = "ls /root";
+  run.ner( prov_cmd );
+  prov_cmd = "$HOME/.transmute/git/transmute/scripts/initializer " + clusterName;
   run.ner( prov_cmd );
 }
 
