3f7a31306740b4ef81e3b7503e9e8dc466962a16
Eric Olszewski <eolszewski@gmail.com>
2018-05-20 14:34:29 -0500

    Add groups middleware to app.

--- packages/transmute-dashboard/src/store/user/middleware.js
+++ packages/transmute-dashboard/src/store/user/middleware.js
@@ -58,3 +58,101 @@ export const setUserProfile = async (auth, profile) => {
       return error.response;
     });
 };
+
+export const getGroups = async (auth) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .get(`/api/v0/groups`);
+};
+
+export const getGroup = async (auth, groupId) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .get(`/api/v0/groups/${id}`);
+};
+
+export const createGroup = async (auth, profile) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .post(`/api/v0/groups/`);
+};
+
+export const deleteGroup = async (auth, groupId) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .delete(`/api/v0/groups/${id}`);
+};
+
+export const getGroupMembers = async (auth, groupId) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .get(`/api/v0/groups/${id}/users`);
+};
+
+export const addGroupMember = async (auth, groupId, userId) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .put(`/api/v0/groups/${id}/users/${userId}`);
+};
+
+export const removeGroupMember = async (auth, groupId, userId) => {
+  let access_token = await auth.getAccessToken();
+  return axios
+    .create({
+      baseURL: ENVS.PROD,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      }
+    })
+    .delete(`/api/v0/groups/${id}/users/${userId}`);
+};
