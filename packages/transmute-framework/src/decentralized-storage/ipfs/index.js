7708524b6a473bd341ed0727ee5310f2dfe26b61
Eric Olszewski <eolszewski@gmail.com>
2018-05-22 09:29:42 -0500

    Add JSDoc annotations to transmute-framework. (#83)

--- packages/transmute-framework/src/decentralized-storage/ipfs/index.js
+++ packages/transmute-framework/src/decentralized-storage/ipfs/index.js
@@ -1,28 +1,82 @@
+/**
+ * A module for reading and writing objects to ipfs
+ * @module src/decentralized-storage
+ */
+
 const ipfsAPI = require('ipfs-api');
 const bs58 = require('bs58');
 
+/** @class TransmuteIpfsAdapter */
 module.exports = class TransmuteIpfsAdapter {
+  /**
+   * Creates a new TransmuteIpfsAdapter
+   * @constructor
+   * @memberof TransmuteIpfsAdapter
+   * @param {Object} config Config object requiring host, port, and protocol
+   */
   constructor(config) {
     this.ipfs = ipfsAPI(config);
   }
+
+  /**
+   * Returns identity of peer
+   * @function
+   * @memberof TransmuteIpfsAdapter
+   * @name healthy
+   * @returns {Object} Peer identity
+   */
   healthy() {
     return this.ipfs.id();
   }
+
+  /**
+   * Writes and object to IPFS
+   * @function
+   * @memberof TransmuteIpfsAdapter
+   * @name writeObject
+   * @param {Object} obj Object being written
+   * @returns {String} IPFS multihash of object
+   */
   async writeObject(obj) {
     return this.ipfs.object.put({
       Data: new Buffer(JSON.stringify(obj)),
       Links: []
     });
   }
+
+  /**
+   * Reads object from IPFS
+   * @function
+   * @memberof TransmuteIpfsAdapter
+   * @name readObject
+   * @param {String} multihash IPFS multihash of object
+   * @returns {Object} Object stored in IPFS multihash
+   */
   async readObject(multihash) {
     let data = (await this.ipfs.object.get(multihash)).data.toString();
     return JSON.parse(data);
   }
 
+  /**
+   * Reads object from IPFS
+   * @function
+   * @memberof TransmuteIpfsAdapter
+   * @name bytes32ToMultihash
+   * @param {String} hash bytes32 hash of IPFS multihash
+   * @returns {String} IPFS multihash of bytes32 hash
+   */
   bytes32ToMultihash(hash) {
     return bs58.encode(new Buffer('1220' + hash.slice(2), 'hex'));
   }
 
+  /**
+   * Reads object from IPFS
+   * @function
+   * @memberof TransmuteIpfsAdapter
+   * @name multihashToBytes32
+   * @param {String} ipfshash IPFS multihash of bytes32 hash
+   * @returns {String} bytes32 hash of IPFS multihash
+   */
   multihashToBytes32(ipfshash) {
     return '0x' + new Buffer(bs58.decode(ipfshash).slice(2)).toString('hex');
   }
--- packages/transmute-framework/src/stream-model/index.js
+++ packages/transmute-framework/src/stream-model/index.js
@@ -1,4 +1,19 @@
+/**
+ * A module for applying events to reducers and creating stream models
+ * @module src/stream-model
+ */
+
+/** @class StreamModel */
 module.exports = class StreamModel {
+  /**
+   * Creates a new StreamModel.
+   * @constructor
+   * @memberof StreamModel
+   * @param {EventStore} eventStore - EventStore where events will originate from.
+   * @param {function} filter - Filter function for filtering events.
+   * @param {function} reducer - Reducer function for reducing events to a model.
+   * @param {Object} state - State object that is used with the reducer.
+   */
   constructor(eventStore, filter, reducer, state) {
     eventStore.requireInstance();
     this.eventStore = eventStore;
@@ -11,15 +26,36 @@ module.exports = class StreamModel {
     };
   }
 
+  /**
+   * Applies an event to this StreamModel's reducer
+   * @function
+   * @memberof StreamModel
+   * @name applyEvent
+   * @param {Object} event - An event object.
+   */
   applyEvent(event) {
     this.state.model = this.reducer(this.state.model, event);
     this.state.lastIndex = event.index;
   };
 
+  /**
+   * Applies an array of events to this StreamModel's reducer
+   * @function
+   * @memberof StreamModel
+   * @name applyEvents
+   * @param {Array.<Object>} events - An array event objects.
+   */
   applyEvents(events) {
     events.filter(this.filter).map(this.applyEvent.bind(this));
   };
 
+
+  /**
+   * Checks EventStore for events which have not been applied to the StreamModel and applies them.
+   * @function
+   * @memberof StreamModel
+   * @name sync
+   */
   async sync() {
     const web3 = this.eventStore.web3;
     const eventCount = (await this.eventStore.eventStoreContractInstance.count.call()).toNumber();
