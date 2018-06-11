416c79e5d5229eed0261032d5cc7dbae259b99e5
Josh Cox <josh@transmute.network>
2018-06-11 10:58:42 -0500

    mac_before

--- .ci/mac_before_install.sh
+++ .ci/mac_before_install.sh
@@ -66,16 +66,7 @@ chkdir /var/lib
 chkdir /var/lib/localkube
 chkdir /var/lib/localkube/certs
 chkdir /lib/systemd/system
-chkdir /usr/bin
-chkdir /usr/local/bin
 
-sudo apt-get update
-sudo apt-get install -yqq \
-  autopoint debhelper dh-apparmor intltool-debian jq libmail-sendmail-perl \
-  libselinux1-dev libsepol1-dev libslang2-dev libsys-hostname-long-perl \
-  jq build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev \
-  libselinux1-dev debhelper lsb-release pkg-config po-debconf \
-  autoconf automake autopoint libtool python2.7-dev
 
 if [[ ! -e "$HOME/.local/bin/nsenter" ]]; then
   mknsenter
