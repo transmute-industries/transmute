be3365c0cf67c7e75a7d73536f06092d3448451a
Vladimir Kobets <vladimir.kobets@arilot.com>
2018-08-08 00:03:34 +0300

    init migrate from bash to ansible

--- packages/transmute-cli/src/commands/init/index.js
+++ packages/transmute-cli/src/commands/init/index.js
@@ -3,14 +3,13 @@ const run = require('../runner');
 const path = require('path');
 
 module.exports.k8s = (dryrun, clusterName) => {
-  let prov_cmd = path.join(
-    __dirname,
-    './initializer ' + clusterName
-  );
+  let prov_cmd = 'ansible-playbook --diff -l "localhost" ' +
+    __dirname +
+    '/../../../components/ansible/init.yml -e ' + clusterName;
 
   if (dryrun == 'true') {
     console.info('<--dry run-->');
-  } else {
-    run.shellExec(prov_cmd);
+    prov_cmd = prov_cmd + " --check";
   }
+  run.shellExec(prov_cmd);
 };
