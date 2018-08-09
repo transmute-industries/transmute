fc7f1915248c6a4a02e8b24ed21afd8dc6040537
Orie Steele <orie@ohs.io>
2018-08-09 10:27:50 -0500

    Fix off by 1 in transmute-framework (#186)
    * fix off by 1
    
    * add slack notifications
    
    * cleanup tests
    
    * test
    
    * cleanup
    
    * implement pr feedback
    
    * fix filter return type

--- packages/transmute-framework/src/stream-model/__tests__/applyEvents.spec.js
+++ packages/transmute-framework/src/stream-model/__tests__/applyEvents.spec.js
@@ -14,6 +14,18 @@ const events = require('../../__mock__/events.json');
 describe('applyEvents', () => {
   let accounts;
   let eventStore;
+  let streamModel;
+
+  const filter = event => {
+    return true;
+  };
+  const reducer = (state, event) => {
+    return {
+      ...state,
+      events: [event.value, ...(state.events || [])],
+      eventsProcessed: (state.eventsProcessed || 0) + 1
+    };
+  };
 
   beforeAll(async () => {
     eventStore = new EventStore({
@@ -27,33 +39,21 @@ describe('applyEvents', () => {
   // new event store per test
   beforeEach(async () => {
     eventStore = await eventStore.clone(accounts[0]);
+    streamModel = new StreamModel(eventStore, filter, reducer);
   });
 
-  it('idempotent', async () => {
-    const filter = event => {
-      return true;
-    };
-    const reducer = (state, event) => {
-      return state;
-    };
-    const streamModel = new StreamModel(eventStore, filter, reducer);
+  it('should be idempotent', async () => {
     streamModel.applyEvents(events);
-    expect(streamModel.state.contractAddress).toEqual(
-      eventStore.eventStoreContractInstance.address
-    );
+    expect(streamModel.state.model).toMatchSnapshot();
+    streamModel.applyEvents(events);
+    expect(streamModel.state.model).toMatchSnapshot();
   });
 
-  it('respects filter', async () => {
-    const filter = event => {
+  it('should support custom event filters', async () => {
+    const custom_filter = event => {
       return event.key.type === 'patient' && event.key.id === '0';
     };
-    const reducer = (state, event) => {
-      return {
-        ...state,
-        events: [...(state.events || []), event]
-      };
-    };
-    const streamModel = new StreamModel(eventStore, filter, reducer);
+    const streamModel = new StreamModel(eventStore, custom_filter, reducer);
     streamModel.applyEvents(events);
     expect(streamModel.state.model).toMatchSnapshot();
   });
--- packages/transmute-framework/truffle-config.js
+++ packages/transmute-framework/truffle-config.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
 const Web3 = require('web3');
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require('truffle-hdwallet-provider');
 const transmuteConfig = require('./src/transmute-config');
 
 module.exports = {
   migrations_directory: './migrations',
   networks: {
     development: {
-      provider: new Web3.providers.HttpProvider(
-        transmuteConfig.web3Config.providerUrl
-      ),
+      provider: function() {
+        return new Web3.providers.HttpProvider(
+          transmuteConfig.web3Config.providerUrl
+        );
+      },
       network_id: '*' // Match any network id
     },
     // Providers using HDWallet need to be wrapped in a function: https://truffleframework.com/docs/advanced/configuration#providers
     rinkeby: {
-      provider: function () {
-        return new HDWalletProvider(process.env.TESTNET_MNEMONIC, `https://rinkeby.infura.io/${process.env.INFURA_API_TOKEN}`);
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.TESTNET_MNEMONIC,
+          `https://rinkeby.infura.io/${process.env.INFURA_API_TOKEN}`
+        );
       },
       network_id: 4
     }
