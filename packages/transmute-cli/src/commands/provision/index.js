93d17f7ac7d3f305e86c63360fea8946e7d4ab56
luenix <jared@transmute.industries>
2018-08-31 16:55:20 -0500

    Fix spacing and fix typo that caused bug with build.

--- packages/transmute-cli/src/commands/provision/index.js
+++ packages/transmute-cli/src/commands/provision/index.js
@@ -24,7 +24,11 @@ module.exports.minikube = (
     MINIKUBE_PROFILE;
 
   if (MINIKUBE_DRIVERS.indexOf(minikubeDriver) == -1) {
-    let os_platform = process.env.platform;
+    let os_platform = process.platform;
+    logger.log({
+      level: 'info',
+      message: `OS platform detected as: "${os_platform}"`,
+    })
 
     if (os_platform != 'linux') {
       minikubeDriver = 'virtualbox';
@@ -56,62 +60,62 @@ module.exports.minikube = (
 };
 
 module.exports.aks = (
-    dryrun,
-    myResourceGroup,
-    myAKSCluster,
-    myNodeCount,
-    myNodeSize,
-    GenSSHKeys,
+  dryrun,
+  myResourceGroup,
+  myAKSCluster,
+  myNodeCount,
+  myNodeSize,
+  GenSSHKeys,
 ) => {
-    let prov_cmd_asible =
-        'ansible-playbook --diff -l "localhost" ' +
-        __dirname +
-        '/../../../components/ansible/provision-azure.yml';
+  let prov_cmd_asible =
+    'ansible-playbook --diff -l "localhost" ' +
+    __dirname +
+    '/../../../components/ansible/provision-azure.yml';
 
-    let gensshkeys_opt = '';
-    if (GenSSHKeys) {
-        gensshkeys_opt = '"--generate-ssh-keys"';
-    }
-    if (myAKSCluster == undefined) {
-        throw 'You need to define a clustername';
-    } else {
-        var akscluster_opt = '"--name ' + myAKSCluster + '"';
-    }
-    if (myNodeCount == undefined) {
-        throw 'You need to define a number of nodes';
-    } else {
-        var nodes_opt = '"--node-count ' + myNodeCount + '"';
-    }
-    if (myNodeSize == undefined) {
-        console.warn('no size given using default');
-        var nodesize_opt = '';
-    } else {
-        var nodesize_opt = '"--node-vm-size ' + myNodeSize + '"';
-    }
-    if (myResourceGroup == undefined) {
-        throw 'You need to define a group';
-    } else {
-        var group_opt = '"--resource-group ' + myResourceGroup + '"';
-    }
+  let gensshkeys_opt = '';
+  if (GenSSHKeys) {
+    gensshkeys_opt = '"--generate-ssh-keys"';
+  }
+  if (myAKSCluster == undefined) {
+    throw 'You need to define a clustername';
+  } else {
+    var akscluster_opt = '"--name ' + myAKSCluster + '"';
+  }
+  if (myNodeCount == undefined) {
+    throw 'You need to define a number of nodes';
+  } else {
+    var nodes_opt = '"--node-count ' + myNodeCount + '"';
+  }
+  if (myNodeSize == undefined) {
+    console.warn('no size given using default');
+    var nodesize_opt = '';
+  } else {
+    var nodesize_opt = '"--node-vm-size ' + myNodeSize + '"';
+  }
+  if (myResourceGroup == undefined) {
+    throw 'You need to define a group';
+  } else {
+    var group_opt = '"--resource-group ' + myResourceGroup + '"';
+  }
 
-    let aksParams =
-        ' -e dryrun=' +
-        dryrun +
-        ' -e group_opt=' +
-        group_opt +
-        ' -e akscluster_opt=' +
-        akscluster_opt +
-        ' -e nodes_opt=' +
-        nodes_opt +
-        ' -e nodesize_opt=' +
-        nodesize_opt +
-        ' -e gensshkeys_opt=' +
-        gensshkeys_opt;
+  let aksParams =
+    ' -e dryrun=' +
+    dryrun +
+    ' -e group_opt=' +
+    group_opt +
+    ' -e akscluster_opt=' +
+    akscluster_opt +
+    ' -e nodes_opt=' +
+    nodes_opt +
+    ' -e nodesize_opt=' +
+    nodesize_opt +
+    ' -e gensshkeys_opt=' +
+    gensshkeys_opt;
 
-    let prov_cmd_azure = prov_cmd_asible + aksParams;
-    if (dryrun === 'true') {
-        console.info('<--dry run-->');
-        prov_cmd_azure = prov_cmd_azure + ' --check';
-    }
-    run.shellExec(prov_cmd_azure);
+  let prov_cmd_azure = prov_cmd_asible + aksParams;
+  if (dryrun === 'true') {
+    console.info('<--dry run-->');
+    prov_cmd_azure = prov_cmd_azure + ' --check';
+  }
+  run.shellExec(prov_cmd_azure);
 };
