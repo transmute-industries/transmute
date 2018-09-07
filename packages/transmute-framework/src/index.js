7e25df960ac33db2c7bda25673a8ec66698af659
Orie Steele <orie@transmute.industries>
2018-09-06 19:48:25 -0500

    cleanup framework deps

--- packages/transmute-framework/src/index.js
+++ packages/transmute-framework/src/index.js
@@ -1,9 +1,8 @@
 /** @module TransmuteFramework */
 
-const EventStoreFactory = require('./event-store-factory');
-const EventStore = require('./event-store');
-const StreamModel = require('./stream-model');
-
+const EventStoreFactory = require("./event-store-factory");
+const EventStore = require("./event-store");
+const StreamModel = require("./stream-model");
 
 module.exports = {
   EventStoreFactory,
