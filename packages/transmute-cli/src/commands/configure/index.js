d8650fca10aaa40b57b99927940b689eb265ff2c
luenix <ppm.jmock@gmail.com>
2018-09-10 11:47:29 -0500

    Fixed whitespace on configure command.

--- packages/transmute-cli/src/commands/configure/index.js
+++ packages/transmute-cli/src/commands/configure/index.js
@@ -3,14 +3,14 @@ const run = require('../runner');
 module.exports.minikube_hosts = (dryrun) => {
   if (dryrun === 'true') {
     console.info('<--dry run-->');
-    run.shellExec('echo $(minikube ip)  transmute.minikube');
-    run.shellExec('echo $(minikube ip)  ipfs.transmute.minikube');
-    run.shellExec('echo $(minikube ip)  ganache.transmute.minikube');
+    run.shellExec('echo $(minikube ip) transmute.minikube');
+    run.shellExec('echo $(minikube ip) ipfs.transmute.minikube');
+    run.shellExec('echo $(minikube ip) ganache.transmute.minikube');
   } else {
     run.shellExec('echo "" | sudo tee -a /etc/hosts');
-    run.shellExec('echo "$(minikube ip)  transmute.minikube" | sudo tee -a /etc/hosts');
-    run.shellExec('echo "$(minikube ip)  ipfs.transmute.minikube" | sudo tee -a /etc/hosts');
-    run.shellExec('echo "$(minikube ip)  ganache.transmute.minikube" | sudo tee -a /etc/hosts');
+    run.shellExec('echo "$(minikube ip) transmute.minikube" | sudo tee -a /etc/hosts');
+    run.shellExec('echo "$(minikube ip) ipfs.transmute.minikube" | sudo tee -a /etc/hosts');
+    run.shellExec('echo "$(minikube ip) ganache.transmute.minikube" | sudo tee -a /etc/hosts');
     run.shellExec('echo "" | sudo tee -a /etc/hosts');
   }
 };
