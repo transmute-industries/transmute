4cbd52bb4ae74b07683be7cb671d8331174e122c
Vladimir Kobets <vladimir.kobets@arilot.com>
2018-09-13 22:01:36 +0300

    fix terraform backend

--- packages/transmute-cli/components/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform.${var.cluster-name}"
-    key    = "terraform.tfstate"
-    region = "${var.region}"
-    access_key = "${var.aws_key}"
-    secret_key = "${var.aws_secret}"
-  }
-}
