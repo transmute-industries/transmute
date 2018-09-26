ceb222a1eea25017276c425182861fc36fe29b3d
Rommel Castro <rommelxcastro@gmail.com>
2018-09-26 20:39:05 +0800

    Updates coverage script.

--- packages/transmute-framework/jest.config.js
+++ packages/transmute-framework/jest.config.js
@@ -21,15 +21,22 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/'],
+  collectCoverageFrom: [
+      '!**/__mocks__/**',
+	  '!**/__tests__/**',
+      '!**/node_modules/**',
+      'src/**/*.js'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+	  '/__mocks__/',
+	  '/__tests__/',
+	  '/node_modules/',
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
