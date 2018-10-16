87c5b14e6504a60e24ab8f4a166374d025207207
gjgd <gjgd@protonmail.com>
2018-10-16 12:32:11 -0500

    Stop using the contract's state as storage for TransmuteEvents. Only uses Solidity events logging feature now

--- packages/transmute-framework/contracts/EventStore.sol
+++ packages/transmute-framework/contracts/EventStore.sol
@@ -1,39 +1,17 @@
 pragma solidity ^0.4.19;
 
-import "./EventStoreLib.sol";
-
 contract EventStore {
+    uint public count;
 
-    EventStoreLib.TransmuteStorage store;
+    event TransmuteEvent(
+        uint index,
+        address indexed sender,
+        bytes32 key,
+        bytes32 value,
+    );
 
-    address public owner;
-
-    function () public payable { revert(); }
- 
-    constructor() public {
-        owner = tx.origin;
-    }
-
-    function count() public view 
-    returns (uint) {
-        return store.events.length;
-    }
-
-    function write(bytes32 key, bytes32 value) public {
-        EventStoreLib.write(
-            store,
-            key,
-            value
-        );
-    }
-    
-    function read(uint index) public view
-    returns (uint, address, bytes32, bytes32 ) {
-        return EventStoreLib.read(store, index);
-    }
-
-    function destroy(address target) public {
-        require(msg.sender == owner);
-        selfdestruct(target);
+    function write(bytes32 key, bytes32 value) {
+        emit TransmuteEvent(count, msg.sender, key, value);
+        count++;
     }
 }
