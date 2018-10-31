6d6d246fc411ff3071338dfc60f62dd929696765
gjgd <gjgd@protonmail.com>
2018-10-31 20:26:45 +0100

    Revert "Fix event-store-adapters tests"
    This reverts commit 84a7be44d36684d744581f6d944da29aa00c23d8.

--- packages/transmute-framework/src/event-store-adapters/__tests__/adapter-ipfs.spec.js
+++ packages/transmute-framework/src/event-store-adapters/__tests__/adapter-ipfs.spec.js
@@ -1,33 +1,32 @@
 const Web3 = require('web3');
 const TransmuteAdapterIPFS = require('transmute-adapter-ipfs');
 const EventStore = require('../../event-store');
+const abi = require('../../../build/contracts/EventStore.json');
 const transmuteConfig = require('../../transmute-config');
-const contractJson = require('../../../build/contracts/EventStore.json');
+
 const mockEvents = require('../../__mock__/events.json');
 
-const provider = new Web3.providers.HttpProvider(transmuteConfig.web3Config.providerUrl);
+const provider = new Web3.providers.HttpProvider(
+  transmuteConfig.web3Config.providerUrl,
+);
 const web3 = new Web3(provider);
 const adapter = new TransmuteAdapterIPFS(transmuteConfig.ipfsConfig);
-const { abi, networks } = contractJson;
-const latestDeploy = Object.keys(networks).pop();
-const { address } = networks[latestDeploy];
-const eventStore = new EventStore({
+let eventStore = new EventStore({
   web3,
   abi,
-  address,
   adapter,
 });
+let accounts;
 
-// TODO: add test for adapter Firebase RTDB
 describe('EventStore with IPFS Adapter', () => {
-  let accounts;
-
   beforeAll(async () => {
-    accounts = await web3.eth.getAccounts();
+    accounts = await eventStore.getWeb3Accounts();
+    eventStore = await eventStore.clone(accounts[0]);
   });
 
   it('supports the ipfs adapter', async () => {
     expect(eventStore).toBeDefined();
+    await eventStore.init();
 
     const writeEventResult = await eventStore.write(
       accounts[0],
@@ -38,8 +37,7 @@ describe('EventStore with IPFS Adapter', () => {
     expect(writeEventResult.event).toBeDefined();
     expect(writeEventResult.meta).toBeDefined();
 
-    const { index } = writeEventResult.event;
-    const readEventResult = await eventStore.read(index);
+    const readEventResult = await eventStore.read(0);
     expect(readEventResult.content.key).toEqual(mockEvents[0].key);
     expect(readEventResult.content.value).toEqual(mockEvents[0].value);
   });
