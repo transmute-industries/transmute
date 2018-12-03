4949e0a01977b69329b6b2de891fed11d301d4ca
gjgd <gjgd@protonmail.com>
2018-12-03 18:13:26 +0100

    Remove signatures from fixtures

--- packages/transmute-did/src/lib/wallet/__tests__/wallet.signatures.spec.js
+++ packages/transmute-did/src/lib/wallet/__tests__/wallet.signatures.spec.js
@@ -1,18 +1,12 @@
+const tmp = require('tmp');
 const fs = require('fs');
 const path = require('path');
 const didWallet = require('../index');
-
 const fullWalletPath = path.resolve(__dirname, '__fixtures__/wallet.plaintext.full.json');
 
-const openpgpDIDDocPath = path.resolve(__dirname, '__fixtures__/openpgp.did.document.json');
-
+const openpgpDIDDocPath = tmp.fileSync().name;
 const openPGPSignaturePath = path.resolve(__dirname, '__fixtures__/wallet.signature.json');
-
-const walletOpenPGPSignaturePath = path.resolve(
-  __dirname,
-  '__fixtures__/wallet.did.openpgp.signature.json',
-);
-
+const walletOpenPGPSignaturePath = tmp.fileSync().name;
 const walletLibSodiumSignaturePath = path.resolve(
   __dirname,
   '__fixtures__/wallet.did.sodium.signature.json',
