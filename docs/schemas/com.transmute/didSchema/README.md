ed7177a442f0a1309b1b77d250eff6f0b84f0344
Orie Steele <orie@ohs.io>
2018-12-10 16:18:16 -0600

    update schemas

--- docs/schemas/com.transmute/didSchema/README.md
+++
@@ -1,5 +0,0 @@
-# DID
-
-> Decentralized Identifiers (DIDs) are a new type of identifier for verifiable, "self-sovereign" digital identity. DIDs are fully under the control of the DID subject, independent from any centralized registry, identity provider, or certificate authority. DIDs are URLs that relate a DID subject to means for trustable interactions with that subject. DIDs resolve to DID Documents — simple documents that describe how to use that specific DID. Each DID Document contains at least three things: cryptographic material, authentication suites, and service endpoints. Cryptographic material combined with authentication suites provide a set of mechanisms to authenticate as the DID subject (e.g., public keys, pseudonymous biometric protocols, etc.). Service endpoints enable trusted interactions with the DID subject.
-
-[W3C DID Spec](https://w3c-ccg.github.io/did-spec/)
--- packages/transmute-did/src/json-schemas/index.js
+++ packages/transmute-did/src/json-schemas/index.js
@@ -1,29 +1,8 @@
-
-const { values } = require('lodash');
-
 const schemas = require('./schemas');
 
 const SchemaValidator = require('./SchemaValidator');
 
 const validator = new SchemaValidator();
-
-// MOVE TO A SCRIPT...  
-// const path = require('path');
-// const writeSchema = require('./writeSchema');
-// const publishSchemas = () => {
-//   const ignoreSchemas = [
-//     'http://json-schema.org/draft-04/schema#',
-//     'https://docs.transmute.industries/schemas/com.transmute.self-desc/schema/jsonschema/1-0-0#',
-//   ];
-//   const schemaBase = path.resolve(__dirname, '../../../../docs/schemas');
-
-//   values(schemas).forEach((s) => {
-//     if (ignoreSchemas.indexOf(s.id || s.$id) === -1) {
-//       //   eslint-disable-next-line
-//       writeSchema(schemaBase, s);
-//     }
-//   });
-// };
 
 const schemaToURI = (selfDescJsonSchema) => {
   const baseURI = 'https://docs.transmute.industries/schemas';
@@ -37,5 +16,4 @@ module.exports = {
   schemaToURI,
   SchemaValidator,
   validator,
-  // publishSchemas,
 };
