49e3f08b2f49454a5e746b7f0fa1fc76208bfcd8
Orie Steele <orie@ohs.io>
2019-01-12 12:32:44 -0600

    add proposed-OpenPgpSignature2019

---
+++ packages/transmute-did/src/lib/__tests__/OpenPgpSignature2019/__fixtures__/index.js
@@ -0,0 +1,9 @@
+const passphrase = 'correct horse battery staple';
+const email = 'alice@example.com';
+const linkedData = require('./linkedData.json');
+
+module.exports = {
+  email,
+  passphrase,
+  linkedData,
+};
