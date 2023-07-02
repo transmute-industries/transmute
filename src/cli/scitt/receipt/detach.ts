0dc1e4d6b1e11ae1d900b73dbec7ddc8b198d11a
Orie Steele <orie@transmute.industries>
2023-07-02 10:15:27 -0500

    Add detach api

--- src/cli/scitt/receipt/detach.ts
+++ src/cli/scitt/receipt/detach.ts
@@ -1,15 +1,29 @@
+import fs from "fs"
+import path from "path"
 
-// scitt specific options
-interface RequestReceipt {
-  issuerKey: string // relative path to private key in jwk format
-  input: string // path to input file
-  output: string // path to output file
+import cose from "@transmute/cose"
+
+
+interface RequestDetachReceipt {
+  signedStatement: string // path to signed statement
+  receipt: string // path to signed inclusion proof
+  transparentStatement: string // path to transparent statement
 }
 
-const detach = async (argv: RequestReceipt) => {
-  const { issuerKey, input, output } = argv
-  console.error('Not implemented')
-
+const detach = async (argv: RequestDetachReceipt) => {
+  const transparentStatement = fs.readFileSync(path.resolve(process.cwd(), argv.transparentStatement))
+  const transparentStatementUnprotectedHeader = cose.unprotectedHeader.get(transparentStatement)
+  const receipt = transparentStatementUnprotectedHeader.get(cose.unprotectedHeader.receipt) as Buffer
+  transparentStatementUnprotectedHeader.delete(cose.unprotectedHeader.receipt)
+  const signedStatement = cose.unprotectedHeader.set(transparentStatement, transparentStatementUnprotectedHeader)
+  fs.writeFileSync(
+    path.resolve(process.cwd(), argv.signedStatement),
+    Buffer.from(signedStatement)
+  )
+  fs.writeFileSync(
+    path.resolve(process.cwd(), argv.receipt),
+    Buffer.from(receipt)
+  )
 }
 
 export default detach
