81cf0466695819f852e984ff5a84fbed8f657331
Orie Steele <orie@transmute.industries>
2024-08-06 15:01:53 -0500

    better enveloped presentation tests

--- tests/traceable-presentations.test.ts
+++ tests/traceable-presentations.test.ts
@@ -1,66 +1,99 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import operationSwitch from "../src/operationSwitch";
 
-import fs from 'fs'
-
-const context = [
-  "https://www.w3.org/ns/credentials/v2",
-  {
-    "replaces": {
-      "@id": "https://w3id.org/traceability#replaces",
-      "@type": "@id"
-    },
-    "patch": {
-      "@id": "https://datatracker.ietf.org/doc/rfc6902#section-4",
-      "@type": "@json"
-    },
-    "workflow": {
-      "@context": {
-        "definition": {
-          "@id": "https://w3id.org/traceability#definition",
-          "@type": "@id"
-        },
-        "instance": {
-          "@id": "https://w3id.org/traceability#instance",
-          "@type": "@id"
-        }
-      }
-    }
-  }
-]
-const type = [
-  "VerifiablePresentation",
-  "TraceablePresentation"
-]
-const workflow = {
-  "definition": [
-    "urn:definition:steel-import"
-  ],
-  "instance": [
-    "urn:instance:steel-import-from-customs-broker-123"
-  ]
-}
+import * as jose from 'jose'
+import * as transmute from "@transmute/verifiable-credentials";
 
 it.skip("simple", async () => {
-  const presentationWithErrors = { '@context': context, id: 'urn:uuid:1111', type, workflow, name: 'alcie' }
-  const presentationWithCorrections = {
-    '@context': context, id: 'urn:uuid:2222', replaces: {
-      id: 'urn:uuid:1111',
-      type
-    }, type, workflow, name: 'alice'
-  }
+  const alg = 'ES256'
+  const issuer = 'did:web:gbi.vendor.example'
+  const issuerPrivateKey = await transmute.key.generate({
+    alg,
+    type: "application/jwk+json",
+  });
+  const subject = 'did:web:logistics.example'
+  const holderPrivateKey = await transmute.key.generate({
+    alg,
+    type: "application/jwk+json",
+  });
+
+  const issued = await transmute
+    .issuer({
+      alg,
+      type: "application/vc+ld+json+jwt",
+      signer: {
+        sign: async (bytes: Uint8Array) => {
+          const jws = await new jose.CompactSign(bytes)
+            .setProtectedHeader({ kid: `${issuer}#assertion-key`, alg })
+            .sign(
+              await transmute.key.importKeyLike({
+                type: "application/jwk+json",
+                content: issuerPrivateKey,
+              })
+            );
+          return transmute.text.encoder.encode(jws);
+        },
+      },
+    })
+    .issue({
+      claimset: transmute.text.encoder.encode(`
+"@context":
+  - https://www.w3.org/ns/credentials/v2
+id: urn:uuid:f9e95eb8-1ed0-46b8-a6bf-c46539aef6c9
+type:
+  - VerifiableCredential
+issuer:
+  id: ${issuer}
+  name: "GBI Issuer"
+validFrom: 2024-08-06T19:35:57.505Z
+credentialSubject:
+  id: ${subject}
+`),
+    });
+
+  const presentation = await transmute
+    .holder({
+      alg,
+      type: "application/vp+ld+json+jwt",
+    })
+    .issue({
+      signer: {
+        sign: async (bytes: Uint8Array) => {
+          const jws = await new jose.CompactSign(bytes)
+            .setProtectedHeader({ kid: `${subject}#authentication-key`, alg })
+            .sign(
+              await transmute.key.importKeyLike({
+                type: "application/jwk+json",
+                content: holderPrivateKey,
+              })
+            );
+          return transmute.text.encoder.encode(jws);
+        },
+      },
+      presentation: {
+        "@context": ["https://www.w3.org/ns/credentials/v2"],
+        type: ["VerifiablePresentation"],
+        holder: `${subject}`,
+        // this part is built from disclosures without key binding below.
+        // "verifiableCredential": [{
+        //   "@context": "https://www.w3.org/ns/credentials/v2",
+        //   "id": "data:application/vc+ld+json+sd-jwt;QzVjV...RMjU",
+        //   "type": "EnvelopedVerifiableCredential"
+        // }]
+      },
+      disclosures: [
+        {
+          type: `application/vc+ld+json+jwt`,
+          credential: issued,
+        },
+      ],
+    });
+
   await operationSwitch({
-    json: `${JSON.stringify(presentationWithErrors, null, 2)}`,
+    json: `${JSON.stringify({ jwt: new TextDecoder().decode(presentation) }, null, 2)}`,
     neo4jUri: process.env.NEO4J_URI,
     neo4jUser: process.env.NEO4J_USERNAME,
     neo4jPassword: process.env.NEO4J_PASSWORD
   });
-  await operationSwitch({
-    json: `${JSON.stringify(presentationWithCorrections, null, 2)}`,
-    neo4jUri: process.env.NEO4J_URI,
-    neo4jUser: process.env.NEO4J_USERNAME,
-    neo4jPassword: process.env.NEO4J_PASSWORD
-  });
-  fs.writeFileSync('examples/neo4j/simple-presentation-with-errors.json', JSON.stringify(presentationWithErrors, null, 2))
-  fs.writeFileSync('examples/neo4j/simple-presentation-making-corrections.json', JSON.stringify(presentationWithCorrections, null, 2))
+
 });
