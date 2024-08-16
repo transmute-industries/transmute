d7394afe21807d353d8f779a365bbad5cc92ecce
Orie Steele <orie@transmute.industries>
2024-08-16 14:43:04 -0500

    order set secrets

--- rotate-action-secrets.sh
+++ rotate-action-secrets.sh
@@ -1,13 +1,13 @@
 source $1
 
-gh secret set NEO4J_URI --body "$NEO4J_URI"
-gh secret set NEO4J_USERNAME --body "$NEO4J_USERNAME"
-gh secret set NEO4J_PASSWORD --body "$NEO4J_PASSWORD"
-
 gh secret set AZURE_TENANT_ID --body "$AZURE_TENANT_ID"
 gh secret set AZURE_CLIENT_ID --body "$AZURE_CLIENT_ID"
 gh secret set AZURE_CLIENT_SECRET --body "$AZURE_CLIENT_SECRET"
 gh secret set AZURE_KEY_ID --body "$AZURE_KEY_ID"
+
+gh secret set NEO4J_URI --body "$NEO4J_URI"
+gh secret set NEO4J_USERNAME --body "$NEO4J_USERNAME"
+gh secret set NEO4J_PASSWORD --body "$NEO4J_PASSWORD"
 
 gh secret set TOKEN_AUDIENCE --body "$TOKEN_AUDIENCE"
 gh secret set TOKEN_ENDPOINT --body "$TOKEN_ENDPOINT"
